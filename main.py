3ed6111e199abeaddd6490a5d84fdcfc67f89ec0
Felix Liang <108435335+PufferfishPond@users.noreply.github.com>
2023-03-22 03:24:06 +0000

    INPUT AND MADLIBS MAKE NO SENSE WHY DOES THE FIRST INPUT BLOCK ALL THE CODE BELOW IT

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 print("hello world")
 print("hi")
+
 print(1+1)
 print(2-1)
 print(2*3)
 print(6/2)
 print("1+1")
 
-vegetable = "apple"
+vegetable = "cabbage"
 print(vegetable)
-fruit = "cabbage"
+fruit = "apple"
 print(fruit)
 print(vegetable,fruit,vegetable)
 meat = "chicken"
 meat = "pork"
 meat = "beef"
 print(meat, meat, meat)
+
+name = input("Felix")
+adjective = input("cool")
+verb = input("sleep")
+place = input("home")
+food = input("sugar")
+vehicle = input("car")
